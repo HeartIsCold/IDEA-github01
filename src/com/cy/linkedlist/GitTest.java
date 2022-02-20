dcbf1cb09a76c106ece066bc87648826732af7d7
liuhubo <liuhubo@guigu.com>
2022-02-20 15:23:42 +0800

    fifth submit

--- src/com/cy/linkedlist/GitTest.java
+++ src/com/cy/linkedlist/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("hellogit1");
         System.out.println("hellogit2");
         System.out.println("master hot-fix both test");
+        System.out.println("master push test");
     }
 }
